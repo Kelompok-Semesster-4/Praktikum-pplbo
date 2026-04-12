af3c554a47fa9cf93073cf46a992a5c31e0d556e
laluomen <mhmmdwldnaz@gmail.com>
2026-04-12 14:47:03 +0700

    FIX: following example for fines in LoanService and use "static" method to calculate fine/

--- perpustakaan/src/main/java/com/library/app/util/FineCalculator.java
+++ perpustakaan/src/main/java/com/library/app/util/FineCalculator.java
@@ -6,14 +6,14 @@ import java.time.LocalDate;
 import com.library.app.model.FineRule;
 
 public class FineCalculator {
-    public BigDecimal calculateTotalFine(long daysLate) {
+    public static BigDecimal calculateTotalFine(long daysLate) {
         if (daysLate <= 0) {
             return BigDecimal.ZERO;
         }
         return new BigDecimal(daysLate * FineRule.getInstance().getFinePerDay());
     }
 
-    public BigDecimal processFine(LocalDate dueDate, LocalDate returnDate) {
+    public static BigDecimal processFine(LocalDate dueDate, LocalDate returnDate) {
         if (!DateUtil.isLoanLate(dueDate, returnDate)) {
             return BigDecimal.ZERO;
         }
